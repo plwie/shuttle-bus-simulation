c297e330c0cdffae15f9469956e3d5f7440010f0
plwie <58283153+plwie@users.noreply.github.com>
2021-03-07 14:47:17 +0700

    Update psg.go

--- lib/psg.go
+++ lib/psg.go
@@ -19,7 +19,7 @@ func NewPassengerAt(BusStop) {
 		p = new(passenger)
 		p.source = BusStop.name
 		rando := rand.Intn(len(allStation))
-		p.destination = BusStop[rando]
+		p.destination = BusStop[rando].name
 		if p.destination == p.source {
 			continue
 		} else if p.destination != p.source {
@@ -36,9 +36,9 @@ func NewPassenger(BusStop) {
 		var p *passenger
 		p = new(passenger)
 		rando1 := rand.Intn(len(allStation))
-		p.source = BusStop[rando1]
+		p.source = BusStop[rando1].name
 		rando2 := rand.Intn(len(allStation))
-		p.destination = BusStop[rando2]
+		p.destination = BusStop[rando2].name
 		if p.destination == p.source {
 			continue
 		} else if p.destination != p.source {
