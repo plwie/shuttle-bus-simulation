4c96ca39ab3253aed2d283370fb4c06ed650e5ef
plwie <pillalince12@gmail.com>
2021-03-07 18:39:54 +0700

    Update psg.go

--- lib/psg.go
+++ lib/psg.go
@@ -2,50 +2,53 @@ package rs
 
 import (
 	"math/rand"
+	"time"
 )
 
 // Passenger create a passenger object
 type Passenger struct {
-	source      string
-	destination string
+	Source      string
+	Destination string
 	next        *Passenger
 }
 
 //NewPassengerAt add passenger to specific bus stop
-func NewPassengerAt(stopList []BusStop, start BusStop) {
-	// passengerGroup := []*Passenger{}
+func NewPassengerAt(stopList []BusStop, start BusStop) []*Passenger {
+	passengerGroup := []*Passenger{}
 	for i := 1; i < rand.Intn(100); i++ {
 		var p *Passenger
 		p = new(Passenger)
-		p.source = start.Name
+		p.Source = start.Name
+		rand.Seed(time.Now().Unix())
 		rando := rand.Intn(len(stopList))
-		p.destination = stopList[rando].Name
-		if p.destination == p.source {
+		p.Destination = stopList[rando].Name
+		if p.Destination == p.Source {
 			continue
-		} else if p.destination != p.source {
-			if p.source == BusStop.Name {
-				BusStop.q.Add(psgr[i])
-			}
+		} else if p.Destination != p.Source {
+			passengerGroup = append(passengerGroup, p)
 		}
 	}
+	return passengerGroup
 }
 
 //NewPassenger add passenger to random bus stop
-func NewPassenger(stopList []BusStop) {
-	// passengerGroup := []*Passenger{}
+func NewPassenger(stopList []BusStop) []*Passenger {
+	passengerGroup := []*Passenger{}
+	// rand.Seed(time.Now().Unix())
 	for i := 1; i < rand.Intn(20); i++ {
 		var p *Passenger
 		p = new(Passenger)
-		rando1 := rand.Intn(len(stopList))
-		p.source = stopList[rando1].Name
-		rando2 := rand.Intn(len(stopList))
-		p.destination = stopList[rando2].Name
-		if p.destination == p.source {
+		rand.Seed(time.Now().Unix())
+		rando1 := rand.Intn(len(stopList) - 1)
+		p.Source = stopList[rando1].Name
+		// rand.Seed(time.Now().Unix())
+		rando2 := rand.Intn(len(stopList) - 1)
+		p.Destination = stopList[rando2].Name
+		if p.Destination == p.Source {
 			continue
-		} else {
-			if p.source == BusStop.Name {
-				BusStop.q.Add(psgr[i])
-			}
+		} else if p.Destination != p.Source {
+			passengerGroup = append(passengerGroup, p)
 		}
 	}
+	return passengerGroup
 }
