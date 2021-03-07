3cd26c69de49dd7aedd67ae7284ff82ba7f7b825
plwie <58283153+plwie@users.noreply.github.com>
2021-03-07 15:01:00 +0700

    Update psg.go

--- lib/psg.go
+++ lib/psg.go
@@ -2,7 +2,7 @@ package rs
 
 import (
 	"math/rand"
-	"fmt"
+	"ftm"
 )
 
 // Passenger create a passenger object
@@ -14,10 +14,10 @@ type Passenger struct {
 
 //NewPassengerAt add passenger to specific bus stop
 func NewPassengerAt(BusStop) {
-	passengerGroup := []*passenger{}
+	passengerGroup := []*Passenger{}
 	for i := 1; i < rand.Intn(100); i++ {
-		var p *passenger
-		p = new(passenger)
+		var p *Passenger
+		p = new(Passenger)
 		p.source = BusStop.name
 		rando := rand.Intn(len(allStation))
 		p.destination = BusStop[rando].name
@@ -32,10 +32,10 @@ func NewPassengerAt(BusStop) {
 
 //NewPassenger add passenger to random bus stop
 func NewPassenger(BusStop) {
-	passengerGroup := []*passenger{}
+	passengerGroup := []*Passenger{}
 	for i := 1; i < rand.Intn(20); i++ {
-		var p *passenger
-		p = new(passenger)
+		var p *Passenger
+		p = new(Passenger)
 		rando1 := rand.Intn(len(allStation))
 		p.source = BusStop[rando1].name
 		rando2 := rand.Intn(len(allStation))
