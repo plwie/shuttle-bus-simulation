a50e770b06beb7888c2228ac92a1bac3ee78e1db
plwie <pillalince12@gmail.com>
2021-03-07 16:09:37 +0700

    Update main.go

--- main.go
+++ main.go
@@ -27,17 +27,18 @@ func main() {
 		go rs.Busc("bus"+fmt.Sprint((i+1)), stopList, count)
 	}
 
-	psgr := rs.NewPassenger(stopList)
+	psgr := rs.NewPassenger(stopList.Name)
 
-	for _, ele := range psgr {
-		for _, ele := range stopList {
-			if psgr.ele.src == stopList.ele.name {
-				stopList.q.Add(psgr(ele))
-			} else if psgr.ele.src != stopList.ele.name {
-				continue
-			}
-		}
-	}
+	fmt.Println(psgr)
+	// for _, u := range psgr {
+	// 	for _, ele := range stopList {
+	// 		if psgr.ele.src == stopList.ele.name {
+	// 			stopList.q.Add(psgr(i))
+	// 		} else if psgr.ele.src != stopList.ele.name {
+	// 			continue
+	// 		}
+	// 	}
+	// }
 
 	rs.Busc("test", stopList, count)
 	fmt.Println("Ending main package...")
