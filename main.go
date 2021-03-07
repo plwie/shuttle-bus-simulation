818e7e2d7b356720bc278310d52af7572bf1e876
Euro Nattanan <53858594+euronattanan@users.noreply.github.com>
2021-03-07 23:46:21 +0700

    Update main.go

--- main.go
+++ main.go
@@ -26,29 +26,29 @@ func main() {
 	stopList = append(stopList, &rs.BusStop{Name: "hBuilding", TimeTaken: 5})
 	stopList = append(stopList, &rs.BusStop{Name: "iBuilding", TimeTaken: 7})
 	stopList = append(stopList, &rs.BusStop{Name: "jBuilding", TimeTaken: 4})
-	// fmt.Printf("Initiated bus stop list: %v\n", stopList)
-	// fmt.Println("This is the main package: ")
-	// fmt.Println("How many bus?")
-	// fmt.Scanln(&inputNoBus)
-	// for i := 0; i < inputNoBus; i++ {
-	// 	go rs.Busc("bus"+fmt.Sprint((i+1)), stopList)
-
+	fmt.Printf("Initiated bus stop list: %v\n", stopList)
+	fmt.Println("This is the main package: ")
+	fmt.Println("How many bus?")
+	fmt.Scanln(&inputNoBus)
+	for i := 0; i < inputNoBus; i++ {
+		go rs.Busc("bus"+fmt.Sprint((i+1)), stopList)
+	}
 	// ------------------------------------------ของปลื้ม-----------------------------------------------
 	// rand.Seed(time.Now().Unix())
 	// rando2 := rand.Intn(len(stopList))
 	// fmt.Println(stopList[rando2].Name)
 	// fmt.Println(len(stopList))
 
-	psgr := rs.NewPassenger(stopList)
+	// psgr := rs.NewPassenger(stopList)
 
-	// fmt.Println(psgr)
-	// fmt.Println(*psgr[0])
-	// fmt.Println(psgr[0])
-	fmt.Println(*&psgr[1].Source)
-	// fmt.Println(*&stopList[2].Name)
-	// fmt.Println(*&stopList[2].Q.Size)
-	fmt.Println(*&stopList[3].Q.Size)
-	fmt.Println(random(1, 10))
+	// // fmt.Println(psgr)
+	// // fmt.Println(*psgr[0])
+	// // fmt.Println(psgr[0])
+	// fmt.Println(*&psgr[1].Source)
+	// // fmt.Println(*&stopList[2].Name)
+	// // fmt.Println(*&stopList[2].Q.Size)
+	// fmt.Println(*&stopList[3].Q.Size)
+	// fmt.Println(random(1, 10))
 
 	// fmt.Println(stopList)
 	// for x := range psgr {
@@ -65,6 +65,6 @@ func main() {
 
 	// ------------------------------------------ของปลื้ม-----------------------------------------------
 
-	// rs.Busc("test", stopList, count)
+	rs.Busc("test", stopList)
 	// fmt.Println("Ending main package...")
 }
