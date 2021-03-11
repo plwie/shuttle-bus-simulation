aafaf912dd3c95ce9bd39069fe526bb9df0efba4
plwie <pillalince12@gmail.com>
2021-03-11 14:56:16 +0700

    Update psg.go

--- lib/psg.go
+++ lib/psg.go
@@ -67,4 +67,19 @@ func NewPassenger1(stopList []*BusStop) *Passenger {
 	return p
 }
 
+//GnrPsg Generate psg and add to bus stop
+func GnrPsg(stopList []*BusStop, random1 int, psgr *Passenger) {
+	for i := 1; i < random1; i++ {
+		psgr.Source = *&stopList[rand.Intn(10)].Name
+		psgr.Destination = *&stopList[rand.Intn((len(stopList)-0-1)+1)].Name
+		for i := 0; i < len(stopList)-1; i++ {
+			if psgr.Source == *&stopList[i].Name {
+				stopList[i].Q.Add(*psgr)
+				// fmt.Println(stopList[i].Name)
+				// fmt.Println(stopList[i].Q.Size)
+			}
+		}
+	}
+}
+
 // Random code from https://gist.github.com/201Factory/5ef7c2d46cf848db16041cafa17ab054
