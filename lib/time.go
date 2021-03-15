b62d9a9d8a3933508bf0e03d86ce9be949069d1a
Tanapol Dangsakul <62011301@kmitl.ac.th>
2021-03-15 21:11:14 +0700

    Edit Time to take pointers

--- lib/time.go
+++ lib/time.go
@@ -1,10 +1,10 @@
 package rs
 
 // TimeTick add 1 min to the global clock
-func TimeTick(hour int, min int) {
-	min++
-	if min >= 60 {
-		hour += min / 60
-		min -= 60
+func timeTick(hour *int, min *int) {
+	*min++
+	if *min >= 60 {
+		*hour += *min / 60
+		*min -= 60 * (*min / 60)
 	}
 }
