6f016e7b8f897a4f2a3596bb9e8205bd0d9fffc2
plwie <pillalince12@gmail.com>
2021-03-15 21:01:09 +0700

    Update car.go

--- lib/car.go
+++ lib/car.go
@@ -11,10 +11,10 @@ type Car struct {
 }
 
 //CarGroup create group of cars
-func CarGroup(cars int) []*Car {
+func CarGroup() []*Car {
 	CarGroup := []*Car{}
 	rand.Seed(time.Now().UnixNano())
-	random1 := random(5, 15)
+	random1 := random(15, 25)
 	for i := 1; i < random1; i++ {
 		var p *Car
 		p = new(Car)
