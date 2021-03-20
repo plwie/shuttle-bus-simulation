83e5fc5560f54e215a7bd0219efb7801d7a92a64
Tanapol Dangsakul <62011301@kmitl.ac.th>
2021-03-20 17:01:24 +0700

    optimized psg.go

---
+++ Testing Code/Pluem/psgBackup1.go
@@ -0,0 +1,104 @@
+package rs
+
+import (
+	"math/rand"
+	"time"
+)
+
+// Passenger create a passenger object
+type Passenger struct {
+	Source      string
+	Destination string
+	next        *Passenger
+	waitingTime int
+}
+
+func random(min int, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+//NewPassengerAt add passenger to specific bus stop
+func NewPassengerAt(stopList []BusStop, start BusStop) []*Passenger {
+	passengerGroup := []*Passenger{}
+	for i := 1; i < rand.Intn(10000); i++ {
+		var p *Passenger
+		p = new(Passenger)
+		p.Source = start.Name
+		rand.Seed(time.Now().Unix())
+		rando := rand.Intn(len(stopList))
+		p.Destination = stopList[rando].Name
+		if p.Destination == p.Source {
+			continue
+		} else if p.Destination != p.Source {
+			passengerGroup = append(passengerGroup, p)
+		}
+	}
+	return passengerGroup
+}
+
+//NewPassenger add passenger to random bus stop
+func NewPassenger(stopList []*BusStop) []*Passenger {
+	passengerGroup := []*Passenger{}
+	rand.Seed(time.Now().UnixNano())
+	random1 := random(3000, 10000)
+	for i := 1; i < random1; i++ {
+		var p *Passenger
+		p = new(Passenger)
+		rand.Seed(time.Now().UnixNano())
+		rando1 := random(1, 5)
+		p.Source = stopList[rando1].Name
+		rand.Seed(time.Now().UnixNano())
+		rando2 := random(1, 5)
+		p.Destination = stopList[rando2].Name
+		if p.Destination == p.Source {
+			continue
+		} else if p.Destination != p.Source {
+			passengerGroup = append(passengerGroup, p)
+		}
+	}
+	return passengerGroup
+}
+
+//-----------------------------------------------------------------------------------
+
+//NewPassenger1 Create a new passenger
+func NewPassenger1() *Passenger {
+	var p *Passenger
+	p = new(Passenger)
+	return p
+}
+
+//GnrPsg Generate psg and add to bus stop
+func GnrPsg(stopList []*BusStop, random1 int, psgr *Passenger) {
+	for i := 1; i < random1; i++ {
+		psgr.Source = *&stopList[rand.Intn(10)].Name
+		psgr.Destination = *&stopList[rand.Intn((len(stopList)-0-1)+1)].Name
+		for i := 0; i < len(stopList)-1; i++ {
+			if psgr.Source == *&stopList[i].Name {
+				stopList[i].Q.Add(*psgr)
+				// fmt.Println(stopList[i].Name)
+				// fmt.Println(stopList[i].Q.Size)
+			}
+		}
+	}
+}
+
+//GnrPsgAt Generate psg and add to specific bus stop
+func GnrPsgAt(stopList []*BusStop, stop string, random1 int, psgr *Passenger) {
+	for i := 1; i < random1; i++ {
+		for i := 0; i < len(stopList)-1; i++ {
+			if stop == *&stopList[i].Name {
+				stopList[i].Q.Add(*psgr)
+				// fmt.Println(stopList[i].Name)
+				// fmt.Println(stopList[i].Q.Size)
+			}
+		}
+	}
+}
+
+//Random number of int
+func Random(min int, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+// Random code from https://gist.github.com/201Factory/5ef7c2d46cf848db16041cafa17ab054
--- test.go
+++ test.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	rs "rs/lib"
+	"strconv"
 	"strings"
 )
 
@@ -11,14 +13,82 @@ var (
 	mainInput string
 	mainCmd   []string
 	err       error
+	tStopLst  []*rs.BusStop
+	tMin      int
+	tHr       int
 )
 
-func testA() {
-	fmt.Println("Succesfully called function A...")
+func bstPrint() bool {
+	// This function should print out the list of created bus stop
+	if len(tStopLst) == 0 {
+		fmt.Println("Error: no item in bus stop list")
+		return false
+	}
+	fmt.Printf("List of bus stops: ")
+	for _, v := range tStopLst {
+		fmt.Printf("%v ", v)
+	}
+	fmt.Printf("\n")
+	return true
 }
 
-func testD() {
-	fmt.Println("Succesfully called function D...")
+func bstEnqueue() bool {
+	// This function should add psg into queue of target bus stop
+	success := false
+	// Convert second parameter into int
+	psgNum, err := strconv.Atoi(mainCmd[2])
+	if err != nil {
+		fmt.Printf("Error: invalid argument %v\n", mainCmd[2])
+		return success
+	}
+	// Create a new passenger
+	p := rs.NewPassenger1(tStopLst)
+	for i := 0; i < j; i++ {
+		rs.GnrPsgAt(tStopLst, mainCmd[1], psgNum, p)
+
+	}
+
+	if success {
+		fmt.Println("Added successsfully")
+	} else {
+		fmt.Println("Error: adding failed")
+	}
+	return success
+}
+
+func bstDequeue() bool {
+	// This function should remove psg out of queue of target bus stop
+	success := false
+
+	if success {
+		fmt.Println("Removed successsfully")
+	} else {
+		fmt.Println("Error: removing failed")
+	}
+	return success
+}
+
+func tick() bool {
+	tMin = 0
+	tHr = 0
+	for tHr < 24 {
+		fmt.Printf("%02v:%02v\n", tHr, tMin)
+		rs.TimeTick(&tHr, &tMin)
+	}
+	fmt.Printf("%02v:%02v\n", tHr, tMin)
+	return true
+}
+
+func help() bool {
+	fmt.Println("bstList				-> Print out the list of created bus stop")
+	fmt.Println("bstCreate name 		-> Create a new bus stop with a name")
+	fmt.Println("bstAdd tgStop psgNum 	-> Add psgNum passengers into the tgStop bus stop")
+	fmt.Println("bstRmv tgStop psgNum 	-> Remove psgNum passengers into the tgStop bus stop")
+	fmt.Println("timeTick 				-> Run 1 day of time tick and print the clock")
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("")
+	return true
 }
 
 func main() {
@@ -26,9 +96,12 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	// text = strings.Replace(text, "\n", "", -1)
 
-	var cmdMap = map[string]func(){
-		"FunctionA": testA,
-		"FunctionD": testD,
+	var cmdMap = map[string](func() bool){
+		"bstList": bstPrint,
+		"bstAdd":  bstEnqueue,
+		"bstRmv":  bstDequeue,
+		"runDay":  tick,
+		"help":    help,
 	}
 
 	fmt.Println(cmdMap)
