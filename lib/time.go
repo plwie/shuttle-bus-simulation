9abb74d344a3704abdb0ca3fa201cb93bfdb215b
Tanapol Dangsakul <62011301@kmitl.ac.th>
2021-03-21 21:49:24 +0700

    Update time.go

--- lib/time.go
+++ lib/time.go
@@ -12,34 +12,42 @@ func TimeTick(hour *int, min *int) {
 		*hour += *min / 60
 		*min -= 60 * (*min / 60)
 	}
-	fmt.Printf("%02v:%02v\n", *hour, *min)
+	// fmt.Printf("%02v:%02v\n", *hour, *min)
 }
 
 // ConTimeTick constantly call Time Tick
-func ConTimeTick(hour *int, min *int, lst []*BusStop, p *Passenger) {
+func ConTimeTick(hour *int, min *int, lst []*BusStop, p *Passenger) int {
+	eventCalled := 0
+	initTime := 0
 	for *hour < 24 {
+		fmt.Printf("%02v:%02v\n", *hour, *min)
 		TimeTick(hour, min)
 		time.Sleep(time.Millisecond * 1)
-		initTime := 0
-		// // Event Class End --------------------------------------------
-		if *hour >= 0 {
-			if initTime != *hour {
-				initTime = *hour
-			}
+		// Event Class End --------------------------------------------
+		if initTime != *hour {
 			GnrPsg(lst, Random(150, 200), p)
+			fmt.Println("case1")
+			eventCalled++
+			initTime++
 			//rs.GnrTrf(CarGroup())
 		}
 
 		// Event train ----------------------------------------
-		if (*hour) == 2 && *min >= 0 {
-			GnrPsgAt(lst, "hBuilding", Random(50, 100), p)
+		if *hour%2 == 0 && *min == 0 {
+			GnrPsgAt(lst, "b", Random(50, 100), p)
+			fmt.Println("case2")
+			eventCalled++
 			//rs.GnrTrf(CarGroupTr())
 		}
 
 		// // Event After 4pm ---------------------------------------------
-		if (*hour == 16 || *hour == 17 || *hour == 18) && *min >= 0 {
+		if (*hour == 16 || *hour == 17 || *hour == 18) && *min == 0 {
 			GnrPsg(lst, Random(150, 200), p)
+			fmt.Println("case3")
+			eventCalled++
 			//rs.GnrTrf(CarGroupBusy())
 		}
 	}
+	fmt.Println(eventCalled)
+	return eventCalled
 }
