afa4297bb538a47233ec1c577e685aa3e793fd03
plwie <pillalince12@gmail.com>
2021-03-30 01:11:57 +0700

    Update event.go

--- lib/event.go
+++ lib/event.go
@@ -1,5 +1,13 @@
 package rs
 
+import (
+	"sync"
+)
+
+var (
+	mut sync.Mutex
+)
+
 // ClassEnd --------------------------------------------
 func ClassEnd(lst []*BusStop, p *Passenger) {
 	GnrPsg(lst, Random(150, 200), p)
@@ -17,4 +25,18 @@ func Train(lst []*BusStop, p *Passenger) {
 func AfterWork(lst []*BusStop, p *Passenger) {
 	GnrPsg(lst, Random(350, 500), p)
 	//rs.GnrTrf(CarGroupBusy())
+}
+
+func Event(lst []*BusStop, p *Passenger, time int, wg *sync.WaitGroup) {
+	if time != 0 {
+		if time <= 480 && time%120 == 60 {
+			ClassEnd(lst, p)
+		} else if time <= 480 && time%120 == 0 {
+			Train(lst, p)
+			ClassEnd(lst, p)
+		} else if time >= 480 && time%120 == 60 {
+			AfterWork(lst, p)
+		}
+	}
+	wg.Done()
 }
