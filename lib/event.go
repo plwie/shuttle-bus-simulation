1cbc1b72852889acd80700addc1033e7f9ac5d41
plwie <pillalince12@gmail.com>
2021-04-01 22:11:44 +0700

    Update event.go

--- lib/event.go
+++ lib/event.go
@@ -1,6 +1,7 @@
 package rs
 
 import (
+	"fmt"
 	"sync"
 )
 
@@ -12,9 +13,8 @@ var (
 func ClassEnd(graph *Graph, lst []*BusStop, p *Passenger) {
 	n := Random(150, 200)
 	GnrPsg(lst, n, p)
-	// generate all roads
 	graph.GenerateTraffic(CarGroup(), nil, nil)
-	// fmt.Println("Event generate:", n, "Passengers")
+	fmt.Println("Event generate:", n, "Passengers")
 
 }
 
@@ -24,30 +24,28 @@ func Train(graph *Graph, lst []*BusStop, p *Passenger) {
 	n1 := Random(150, 200)
 	GnrPsgAt(lst, "hBuilding", n, p)
 	GnrPsg(lst, n1, p)
-	// generate roads that head to train station
-	graph.GenerateTraffic(CarGroupTr(), lst[7], nil) // lst[7] -> &hBuilding
-	// fmt.Println("Event generate:", n+n1, "Passengers")
+	graph.GenerateTraffic(CarGroupTr(), lst[7], nil)
+	fmt.Println("Event generate:", n+n1, "Passengers")
 }
 
 // // AfterWork ---------------------------------------------
 func AfterWork(graph *Graph, lst []*BusStop, p *Passenger) {
 	n := Random(350, 500)
 	GnrPsg(lst, n, p)
-	// generate all roads
 	graph.GenerateTraffic(CarGroupBusy(), nil, nil)
-	// fmt.Println("Event generate:", n, "Passengers")
+	fmt.Println("Event generate:", n, "Passengers")
 }
 
 func Event(graph *Graph, lst []*BusStop, p *Passenger, time int, wg *sync.WaitGroup) {
 	if time != 0 {
-		if time <= 480 && time%120 == 60 { // 60, 180, 300, 420
-			// fmt.Println("At time:", time)
+		if time <= 480 && time%120 == 60 {
+			fmt.Println("At time:", time)
 			ClassEnd(graph, lst, p)
-		} else if time <= 480 && time%120 == 0 { // (120, 240, 360, 480) x 2
-			// fmt.Println("At time:", time)
+		} else if time <= 480 && time%120 == 0 {
+			fmt.Println("At time:", time)
 			Train(graph, lst, p)
-		} else if time >= 480 && time%120 == 60 { // 540
-			// fmt.Println("At time:", time)
+		} else if time >= 480 && time%120 == 60 {
+			fmt.Println("At time:", time)
 			AfterWork(graph, lst, p)
 		}
 	}
