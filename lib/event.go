8a2b67cafe2838af63ad610c94d5ddffb85e853f
plwie <pillalince12@gmail.com>
2021-04-01 20:08:08 +0700

    event update

--- lib/event.go
+++ lib/event.go
@@ -1,6 +1,7 @@
 package rs
 
 import (
+	"fmt"
 	"sync"
 )
 
@@ -9,33 +10,46 @@ var (
 )
 
 // ClassEnd --------------------------------------------
-func ClassEnd(lst []*BusStop, p *Passenger) {
-	GnrPsg(lst, Random(150, 200), p)
-	//rs.GnrTrf(CarGroup())
+func ClassEnd(graph *Graph, lst []*BusStop, p *Passenger) {
+	n := Random(150, 200)
+	GnrPsg(lst, n, p)
+	// generate all roads
+	graph.GenerateTraffic(CarGroup(), nil, nil)
+
+	fmt.Println("Event generate:", n, "Passengers")
 
 }
 
 // Train ----------------------------------------
-func Train(lst []*BusStop, p *Passenger) {
-	GnrPsgAt(lst, "hBuilding", Random(50, 100), p)
-	//rs.GnrTrf(CarGroupTr())
+func Train(graph *Graph, lst []*BusStop, p *Passenger) {
+	n := Random(50, 100)
+	GnrPsgAt(lst, "hBuilding", n, p)
+	// generate roads that head to train station
+	graph.GenerateTraffic(CarGroupTr(), lst[7], nil) // lst[7] -> &hBuilding
+	fmt.Println("Event generate At HBuilding:", n, "Passengers")
 }
 
 // // AfterWork ---------------------------------------------
-func AfterWork(lst []*BusStop, p *Passenger) {
-	GnrPsg(lst, Random(350, 500), p)
-	//rs.GnrTrf(CarGroupBusy())
+func AfterWork(graph *Graph, lst []*BusStop, p *Passenger) {
+	n := Random(350, 500)
+	GnrPsg(lst, n, p)
+	// generate all roads
+	graph.GenerateTraffic(CarGroupBusy(), nil, nil)
+	fmt.Println("Event generate:", n, "Passengers")
 }
 
-func Event(lst []*BusStop, p *Passenger, time int, wg *sync.WaitGroup) {
+func Event(graph *Graph, lst []*BusStop, p *Passenger, time int, wg *sync.WaitGroup) {
 	if time != 0 {
-		if time <= 480 && time%120 == 60 {
-			ClassEnd(lst, p)
-		} else if time <= 480 && time%120 == 0 {
-			Train(lst, p)
-			ClassEnd(lst, p)
-		} else if time >= 480 && time%120 == 60 {
-			AfterWork(lst, p)
+		if time <= 480 && time%120 == 60 { // 60, 180, 300, 420
+			fmt.Println("At time:", time)
+			ClassEnd(graph, lst, p)
+		} else if time <= 480 && time%120 == 0 { // (120, 240, 360, 480) x 2
+			fmt.Println("At time:", time)
+			Train(graph, lst, p)
+			ClassEnd(graph, lst, p)
+		} else if time >= 480 && time%120 == 60 { // 540
+			fmt.Println("At time:", time)
+			AfterWork(graph, lst, p)
 		}
 	}
 	wg.Done()
