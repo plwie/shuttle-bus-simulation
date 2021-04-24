6c338e6d86f05fe489489c6a9820cbfc073a1551
Tanapol Dangsakul <62011301@kmitl.ac.th>
2021-04-24 15:41:08 +0700

    Test graphic

--- Testing Code/New/graphicTest.go
+++ Testing Code/New/graphicTest.go
@@ -1,11 +1,51 @@
 package main
 
-import "github.com/fogleman/gg"
+import (
+	"log"
+	"time"
+
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
+)
 
 func main() {
-	dc := gg.NewContext(1000, 1000)
-	dc.DrawCircle(500, 500, 400)
-	dc.SetRGB(255, 60, 7)
-	dc.Fill()
-	dc.SavePNG("out.png")
+	// Init termui
+	if err := ui.Init(); err != nil {
+		log.Fatalf("failed to initialize termui: %v", err)
+	}
+	defer ui.Close()
+
+	// Bus Travelling
+	g := widgets.NewGauge()
+	g.Title = "Gold Experience Requiem"
+	g.Percent = 0
+	g.SetRect(0, 12, 50, 15)
+	g.BarColor = ui.ColorRed
+	g.BorderStyle.Fg = ui.ColorWhite
+	g.TitleStyle.Fg = ui.ColorYellow
+
+	// Rendering function
+	draw := func() {
+		ui.Render(g)
+	}
+
+	// Render
+	event := ui.PollEvents()
+	for {
+		select {
+		case e := <-event:
+			switch e.ID {
+			case "q", "<C-c>":
+				return
+			}
+		default:
+			draw()
+			g.Percent += 5
+			if g.Percent >= 100 {
+				g.Percent = 0
+			}
+			time.Sleep(time.Second)
+
+		}
+	}
 }
