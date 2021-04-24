dd9af96b27f8378fcb735187bf3cc2f69d1d5b02
Tanapol Dangsakul <62011301@kmitl.ac.th>
2021-04-24 21:15:32 +0700

    Finished ver

--- mainWUI.go
+++ mainWUI.go
@@ -286,65 +286,6 @@ func main() {
 	drawTimer := func(n int) {
 		tp.Text = "Current Time: " + strconv.Itoa(n/60) + " HR: " + strconv.Itoa(n%60) + " MIN"
 		ui.Render(tp)
-	//List box for psg generating events
-	listBox := func(infoes) {
-		l := widgets.NewList()
-		l.Title = "List"
-		l.Rows = infoes
-		// {
-		// "[0] github.com/gizak/termui/v3",
-		// "[1] [你好，世界](fg:blue)",
-		// "[2] [こんにちは世界](fg:red)",
-		// "[3] [color](fg:white,bg:green) output",
-		// "[4] output.go",
-		// "[5] random_out.go",
-		// "[6] dashboard.go",
-		// "[7] foo",
-		// "[8] bar",
-		// "[9] baz",
-		// }
-		l.TextStyle = ui.NewStyle(ui.ColorYellow)
-		l.WrapText = false
-		l.SetRect(0, 0, 25, 12)
-
-		ui.Render(l)
-
-		previousKey := ""
-		uiEvents := ui.PollEvents()
-		for {
-			e := <-uiEvents
-			switch e.ID {
-			case "q", "<C-c>":
-				return
-			case "j", "<Down>":
-				l.ScrollDown()
-			case "k", "<Up>":
-				l.ScrollUp()
-			case "<C-d>":
-				l.ScrollHalfPageDown()
-			case "<C-u>":
-				l.ScrollHalfPageUp()
-			case "<C-f>":
-				l.ScrollPageDown()
-			case "<C-b>":
-				l.ScrollPageUp()
-			case "g":
-				if previousKey == "g" {
-					l.ScrollTop()
-				}
-			case "<Home>":
-				l.ScrollTop()
-			case "G", "<End>":
-				l.ScrollBottom()
-			}
-
-			if previousKey == "g" {
-				previousKey = ""
-			} else {
-				previousKey = e.ID
-			}
-			ui.Render(l)
-		}
 	}
 
 	// Main simulation step
